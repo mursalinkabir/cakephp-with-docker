7cb0a9c5b6b7f358a4169c6c8335a54121f684af
Mursalin Kabir <mail2mursalin@gmail.com>
2023-02-12 18:05:08 +0900

    viewing article

--- src/config/routes.php
+++ src/config/routes.php
@@ -21,7 +21,6 @@
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 
-use Cake\Http\Middleware\CsrfProtectionMiddleware;
 use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 
@@ -45,45 +44,35 @@ return static function (RouteBuilder $routes) {
      */
     $routes->setRouteClass(DashedRoute::class);
 
-//    $routes->scope('/', function (RouteBuilder $builder) {
-//        /*
-//         * Here, we are connecting '/' (base path) to a controller called 'Pages',
-//         * its action called 'display', and we pass a param to select the view file
-//         * to use (in this case, templates/Pages/home.php)...
-//         */
-//        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-//
-//        /*
-//         * ...and connect the rest of 'Pages' controller's URLs.
-//         */
-//        $builder->connect('/pages/*', 'Pages::display');
-//
-//        /*
-//         * Connect catchall routes for all controllers.
-//         *
-//         * The `fallbacks` method is a shortcut for
-//         *
-//         * ```
-//         * $builder->connect('/{controller}', ['action' => 'index']);
-//         * $builder->connect('/{controller}/{action}/*', []);
-//         * ```
-//         *
-//         * You can remove these routes once you've connected the
-//         * routes you want in your application.
-//         */
-//        $builder->fallbacks();
-//    });
-    $routes->setRouteClass(DashedRoute::class);
     $routes->scope('/', function (RouteBuilder $builder) {
-        // Register scoped middleware for in scopes.
-        $builder->registerMiddleware('csrf', new CsrfProtectionMiddleware([
-            'httpOnly' => true,
-        ]));
-        $builder->applyMiddleware('csrf');
-        $builder->connect('/', ['controller' => 'Tests', 'action' => 'show']);
-        $builder->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+        /*
+         * Here, we are connecting '/' (base path) to a controller called 'Pages',
+         * its action called 'display', and we pass a param to select the view file
+         * to use (in this case, templates/Pages/home.php)...
+         */
+        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+
+        /*
+         * ...and connect the rest of 'Pages' controller's URLs.
+         */
+        $builder->connect('/pages/*', 'Pages::display');
+
+        /*
+         * Connect catchall routes for all controllers.
+         *
+         * The `fallbacks` method is a shortcut for
+         *
+         * ```
+         * $builder->connect('/{controller}', ['action' => 'index']);
+         * $builder->connect('/{controller}/{action}/*', []);
+         * ```
+         *
+         * You can remove these routes once you've connected the
+         * routes you want in your application.
+         */
         $builder->fallbacks();
     });
+
     /*
      * If you need a different set of middleware or none at all,
      * open new scope and define routes there.
