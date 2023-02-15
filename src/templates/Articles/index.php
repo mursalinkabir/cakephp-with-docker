f5ef3caacf1bde5496c4b7311737ed354a049c63
Mursalin Kabir <mail2mursalin@gmail.com>
2023-02-15 23:32:05 +0900

    article delete

--- src/templates/Articles/index.php
+++ src/templates/Articles/index.php
@@ -18,6 +18,14 @@
             <td>
                 <?= $article->created->format(DATE_RFC850) ?>
             </td>
+            <td>
+                <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?>
+                <?= $this->Form->postLink(
+                    'Delete',
+                    ['action' => 'delete', $article->slug],
+                    ['confirm' => 'Are you sure?'])
+                ?>
+            </td>
         </tr>
     <?php endforeach; ?>
 </table>
