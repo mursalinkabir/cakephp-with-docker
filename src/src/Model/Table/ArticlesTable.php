366e12f32a63d51a4103955c629c748f9765f9ca
Mursalin Kabir <mail2mursalin@gmail.com>
2023-02-26 11:34:10 +0900

    Implementing custom finder method for fetching tags

--- src/src/Model/Table/ArticlesTable.php
+++ src/src/Model/Table/ArticlesTable.php
@@ -5,6 +5,8 @@ namespace App\Model\Table;
 use Cake\Event\EventInterface;
 use Cake\ORM\Table;
 use Cake\Utility\Text;
+// the Query class
+use Cake\ORM\Query;
 // the Validator class
 use Cake\Validation\Validator;
 
@@ -14,6 +16,7 @@ class ArticlesTable extends Table
     public function initialize(array $config): void
     {
         $this->addBehavior('Timestamp');
+        //creating many to many relationship with tags
         $this->belongsToMany('Tags');
     }
     //this funtion is called automaticall before every save and update operation
@@ -38,5 +41,33 @@ class ArticlesTable extends Table
             ->minLength('body', 10);
 
         return $validator;
+    }
+//Implementing custom Finder Method
+// The $query argument is a query builder instance.
+// The $options array will contain the 'tags' option we passed
+// to find('tagged') in our controller action.
+    public function findTagged(Query $query, array $options)
+    {
+        $columns = [
+            'Articles.id', 'Articles.user_id', 'Articles.title',
+            'Articles.body', 'Articles.published', 'Articles.created',
+            'Articles.slug',
+        ];
+
+        $query = $query
+            ->select($columns)
+            ->distinct($columns);
+
+        if (empty($options['tags'])) {
+            // If there are no tags provided, find articles that have no tags.
+            $query->leftJoinWith('Tags')
+                ->where(['Tags.title IS' => null]);
+        } else {
+            // Find articles that have one or more of the provided tags.
+            $query->innerJoinWith('Tags')
+                ->where(['Tags.title IN' => $options['tags']]);
+        }
+
+        return $query->group(['Articles.id']);
     }
 }
